8bd28887e00d6f04f74b64b82ad5e77a912626a7
Angel D <angeldeniz11235@gmail.com>
2021-10-22 22:50:12 -0700

    Added some form fields using the flutter_form_builder package to the add-team page.

--- lib/views/page/add_team_page.dart
+++ lib/views/page/add_team_page.dart
@@ -1,12 +1,67 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class AddTeamPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormBuilderState>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Add Team"),
-      ),
-    );
+    return FormBuilder(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              FormBuilderTextField(
+                name: "firstName",
+                decoration: InputDecoration(
+                  hintText: 'First Name',
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
+              FormBuilderTextField(
+                name: "lastName",
+                decoration: InputDecoration(
+                  hintText: 'Last Name',
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
+              FormBuilderDateTimePicker(
+                name: "dateOfBirth",
+              ),
+              TextButton(onPressed: () {}, child: Text("Submit")),
+            ],
+          ),
+        ));
   }
 }
