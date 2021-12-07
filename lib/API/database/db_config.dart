73b6094d93892e35ae2747ddb7fdb8fc8b59e3b3
Angel D <angeldeniz11235@gmail.com>
2021-12-07 12:40:50 -0800

    changed db to remote

--- lib/API/database/db_config.dart
+++ lib/API/database/db_config.dart
@@ -2,12 +2,20 @@ import 'dart:io' show Platform;
 
 //TODO - NEED TO SET UP CASES FOR DEV/PRODUCTION
 class DBconfig {
+  //check if LOCAL_NODE_API is set to true
+  static bool isLocalNodeApi =
+      Platform.environment['LOCAL_NODE_API'] == 'true' ? true : false;
+
   static String get_API_IP() {
     Map<String, String> envVars = Platform.environment;
-    if (envVars['NODE_API_IP'] != null) {
-      return envVars['NODE_API_API']!;
+    if (isLocalNodeApi) {
+      if (envVars['NODE_API_IP'] != null) {
+        return envVars['NODE_API_IP']!;
+      } else {
+        return "10.0.2.2";
+      }
     } else {
-      return "10.0.2.2";
+      return '45.77.86.135'; //ADDRESS TO VULTR SERVER
     }
   }
 
